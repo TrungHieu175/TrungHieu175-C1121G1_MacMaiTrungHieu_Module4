d387c270354f747826bf6897a830592aa5b0872c
TrungHieu175 <ablohaha01@gmail.com>
2022-04-09 17:33:42 +0700

    fix bai 6

--- ss6/bai_tap/bai1/src/main/java/com/example/controller/BlogController.java
+++ ss6/bai_tap/bai1/src/main/java/com/example/controller/BlogController.java
@@ -3,14 +3,19 @@ package com.example.controller;
 import com.example.model.Blog;
 import com.example.service.IBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -19,12 +24,16 @@ public class BlogController {
     private IBlogService iBlogService;
 
     @GetMapping({"", "list"})
-    public String goView(Model model) {
-
-        List<Blog> blogList = iBlogService.findAll();
-
-        model.addAttribute("list", blogList);
-
+    public String goList(Model model, @PageableDefault(value = 2) Pageable pageable, @RequestParam Optional<Integer> keyword) {
+        Integer keywordValue = keyword.orElse(null);
+        Page<Blog> blogPage;
+        if (keyword.isPresent()) {
+            blogPage = iBlogService.findAllById(keywordValue, pageable);
+        } else {
+            blogPage = iBlogService.findAllOderBy(pageable);
+        }
+        model.addAttribute("blogPage", blogPage);
+        model.addAttribute("keywordValue", keywordValue);
         return "list";
     }
 
@@ -46,8 +55,9 @@ public class BlogController {
         iBlogService.save(blog);
         return "redirect:/list";
     }
-    @GetMapping("/delete/{id}")
-    public String deleteBlog(@PathVariable Integer id,Model model){
+
+    @GetMapping("/remove")
+    public String delete(RedirectAttributes redirectAttributes, @RequestParam Integer id) {
         iBlogService.deleteById(id);
         return "redirect:/list";
     }
--- ss6/bai_tap/bai1/src/main/java/com/example/service/impl/BlogServie.java
+++ ss6/bai_tap/bai1/src/main/java/com/example/service/impl/BlogServie.java
@@ -4,6 +4,8 @@ import com.example.model.Blog;
 import com.example.repository.IBlogRepository;
 import com.example.service.IBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -33,5 +35,15 @@ public class BlogServie implements IBlogService {
     @Override
     public Blog findById(Integer id) {
         return iBlogRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Page<Blog> findAllById(Integer keyword, Pageable pageable) {
+        return iBlogRepository.findAllById(keyword, pageable);
+    }
+
+    @Override
+    public Page<Blog> findAllOderBy(Pageable pageable) {
+        return iBlogRepository.findAllByOrderById(pageable);
     }
 }
